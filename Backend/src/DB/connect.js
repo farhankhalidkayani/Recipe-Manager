18f53c56f0bb72b31ccae558434836d2afc9d63c
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-11-13 18:05:23 +0500

    Delete Backend directory and moved it to a different repo

--- Backend/src/DB/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const asyncHandler = require("express-async-handler");
-const connect = asyncHandler(async () => {
-  await mongoose.connect(`${process.env.MONGODB_URI}/recipe`);
-  console.log("DB Connected Successfully");
-});
-
-module.exports = connect;
