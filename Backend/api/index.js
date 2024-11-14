ed5e155c7b5dd70b1e781d9d198dfa1e8310490b
Farhan Khalid <rj.farhan4232@gmail.com>
2024-11-14 18:30:17 +0500

    Making changes to deploy

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const express = require("express");
 const connect = require("../src/DB/connect.js");
 const recipeRoutes = require("../src/Routes/recipe.routes.js");
-const serverless = require("serverless-http");
 
 dotenv.config();
 connect();
@@ -14,7 +13,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/recipes", recipeRoutes);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening at PORT ${process.env.PORT}`);
+});
 
-module.exports = app;
-module.exports.handler = serverless(app);
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello" });
+});
+app.use("/recipes", recipeRoutes);
