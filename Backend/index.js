3f451cfc3a27405e1b26316949c8f4192b2005bf
Farhan Khalid <rj.farhan4232@gmail.com>
2024-11-14 19:18:52 +0500

    Making changes to deploy

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const cors = require("cors");
 const express = require("express");
+const http = require("http");
 const connect = require("./src/DB/connect.js");
 const recipeRoutes = require("./src/Routes/recipe.routes.js");
 
@@ -13,11 +14,12 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening at PORT ${process.env.PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello" });
 });
 app.use("/api/recipes", recipeRoutes);
+
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => {
+  console.log(`Listening at PORT ${process.env.PORT}`);
+});
